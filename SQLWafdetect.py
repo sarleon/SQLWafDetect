37ea731c7f5b7224186812f77dad3a286a96944b
sarleon <sarleon56@hotmail.com>
2017-05-24 13:58:29 +0800

    Initial commit

---
+++ SQLWafdetect.py
@@ -0,0 +1,17 @@
+import optparse
+import requests
+
+
+
+
+
+
+
+
+def main():
+    option_parser  =  optparse.OptionParser()
+    option_parser.add_option('-u','--url',dest='url',action='store',default=None)
+
+
+if __name__ == '__main__':
+    main()
